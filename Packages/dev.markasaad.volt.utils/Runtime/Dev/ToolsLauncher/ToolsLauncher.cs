a50ea5cdbddc7b5e93a4bc5ee8b39dec2210d797
voltonik <markasaadramzy@gmail.com>
2024-08-15 14:47:52 +0300

    Fixed namespace

--- Packages/dev.markasaad.volt.utils/Runtime/Dev/ToolsLauncher/ToolsLauncher.cs
+++ Packages/dev.markasaad.volt.utils/Runtime/Dev/ToolsLauncher/ToolsLauncher.cs
@@ -6,34 +6,36 @@ using UnityEngine.UI;
 
 using Volt.Utils.Extensions;
 
-public class ToolsLauncher : MonoBehaviour {
-    [SerializeField] private Transform m_toolsParent;
-    [SerializeField] private GameObject m_toolPrefab;
-    [SerializeField] private GameObject m_toolsView;
+namespace Volt.Utils.Dev {
+    public class ToolsLauncher : MonoBehaviour {
+        [SerializeField] private Transform m_toolsParent;
+        [SerializeField] private GameObject m_toolPrefab;
+        [SerializeField] private GameObject m_toolsView;
 
-    public Dictionary<string, Action> Tools = new() {
-        { "Console", () => Volt.Utils.Dev.Console.SetOpen(true) }
-    }; 
+        public Dictionary<string, Action> Tools = new() {
+            { "Console", () => Volt.Utils.Dev.Console.SetOpen(true) }
+        }; 
 
-    private void Start() {
-        ReinitializeTools();
-    }
-
-    private void ReinitializeTools() {
-        foreach (var child in m_toolsParent.Children()) {
-            Destroy(child.gameObject);
+        private void Start() {
+            ReinitializeTools();
         }
-        foreach (var tool in Tools) {
-            var b = Instantiate(m_toolPrefab, m_toolsParent).GetComponent<Button>();
-            b.onClick.AddListener(() => {
-                tool.Value.Invoke();
-                m_toolsView.SetActive(false);
-            });
-            b.gameObject.GetComponentInChildren<Text>().text = tool.Key;
-        }
-    }
 
-    public void ToggleLauncher() {
-        m_toolsView.SetActive(!m_toolsView.activeSelf);
+        private void ReinitializeTools() {
+            foreach (var child in m_toolsParent.Children()) {
+                Destroy(child.gameObject);
+            }
+            foreach (var tool in Tools) {
+                var b = Instantiate(m_toolPrefab, m_toolsParent).GetComponent<Button>();
+                b.onClick.AddListener(() => {
+                    tool.Value.Invoke();
+                    m_toolsView.SetActive(false);
+                });
+                b.gameObject.GetComponentInChildren<Text>().text = tool.Key;
+            }
+        }
+
+        public void ToggleLauncher() {
+            m_toolsView.SetActive(!m_toolsView.activeSelf);
+        }
     }
 }
